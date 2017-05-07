75f330392cc43ec0906da9422f356d1664d54d7e
Jeppe Ernst <impala.lowrider@gmail.com>
2017-05-07 12:19:47 +0200

    add LAST-MODIFIED timestamp
    Google calendar is still not updating our event info… :tired_face:

--- _plugins/time.rb
+++ _plugins/time.rb
@@ -3,7 +3,12 @@ require 'active_support/all'
 module Jekyll
   module TimeFilter
     def date_to_utc(input)
-      time = ActiveSupport::TimeZone.new(@@timezone).parse(input)
+      tz = ActiveSupport::TimeZone.new(@@timezone)
+      if input.is_a?(Time) 
+      	time = tz.at(input)
+      elsif input.is_a?(String) 
+      	time = tz.parse(input)
+      end
       return time.in_time_zone('UTC')
     end
 
