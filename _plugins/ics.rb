b34f5f5c9e778cf43fe329ff63166d8355a759b7
Jeppe Ernst <impala.lowrider@gmail.com>
2017-05-08 16:46:18 +0200

    just do a split_string on the entire ICS file
    more concise this way. (not really I just want to have a good lead over
    @KasperChristensen :grinning: )

--- _plugins/ics.rb
+++ _plugins/ics.rb
@@ -1,15 +1,20 @@
 module Jekyll
   module IcsFilter
-    def split_string(input)
+    def split_string(input, length = 75, prepend = " ")
       output = ""
-      prepend = " "
-      length = 75
-      splitArray = input.chars.each_slice(length - prepend.length).map(&:join)
-      splitArray.each_with_index do |string, index|
-      	output += string
-      	if index != splitArray.size - 1
-		  output += "\n" + prepend
-		end
+      splitLines = input.lines
+      splitLines.each do | line | 
+        splitArray = line.chars.each_slice(length - prepend.length).map(&:join)
+        if splitArray.length == 1
+          output += splitArray[0]
+        else
+          splitArray.each_with_index do |string, index|
+            output += string
+            if index != splitArray.size - 1
+              output += "\n" + prepend
+            end
+          end
+        end
       end
       return output
     end
