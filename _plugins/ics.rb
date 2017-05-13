6c7f2c4ea502225b0a8f91db4b950265b7396f15
Jeppe Ernst <impala.lowrider@gmail.com>
2017-05-13 19:16:26 +0200

    handle splitting in the middle of æøå
    Now checking if we have a valid UTF8 string, so we don’t end up with
    special characters being split and Liquid getting pissed at us…

--- _plugins/ics.rb
+++ _plugins/ics.rb
@@ -7,15 +7,27 @@ module Jekyll
         if line.bytesize <= length
           output += line
         else
-          output += line.byteslice(0,length)
-          i = length
+          firstline = utf8_valid(line.byteslice(0,length))
+          output += firstline
+          i = firstline.bytesize       
           while i < line.bytesize do
-            output += "\n" + prepend + line.byteslice(i, length - prepend.bytesize)
-            i = i + length - prepend.bytesize
+            nextline = utf8_valid(line.byteslice(i, length - prepend.bytesize))
+            output += "\n" + prepend + nextline
+            i = i + nextline.bytesize
           end
         end
       end
       return output
+    end
+
+    def utf8_valid(input)
+      if input.valid_encoding?
+        return input
+      end
+      while !input.valid_encoding?
+        input = input.byteslice(0,input.bytesize - 1)
+      end
+      return input
     end
 
     def convert_to_crlf(input)
