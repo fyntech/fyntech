6735732b8c4b5786fd2f8900b3d26cc40505513c
Jeppe Ernst <impala.lowrider@gmail.com>
2017-11-12 19:29:59 +0100

    refactor to increase "Cognitive Complexity"

--- _plugins/meetupFetcher.rb
+++ _plugins/meetupFetcher.rb
@@ -8,52 +8,61 @@ module MeetupFetcher
   		return convertedDate
   	end
 
+  	def getMeetUpGroupEvents(group)
+  		maxRetryTimeout = 50
+		initialRetries = 5
+		begin
+			retries ||= initialRetries
+			return @meetup_api.events({ group_urlname: group })
+		rescue Exception => e
+			if retries > 0
+				sleepTimeout = maxRetryTimeout - (maxRetryTimeout / initialRetries * retries)
+				Jekyll.logger.warn("Warning:", "The MeetupApi failed for \"#{group}\" retrying #{retries} times before giving up… Sleeping for #{sleepTimeout} seconds")
+				sleep(sleepTimeout)
+				retries -= 1
+				retry
+			else
+				Jekyll.logger.error("Error:", "No more retries left, we're going down 💥")
+				raise
+			end
+		end
+  	end
+
+  	def generateEventDocument(event)
+  		doc = Jekyll::Document.new('', :site => @site, :collection => @collection)
+		doc.data['title'] = event['name']
+		doc.data['dateStart'] = convertDate(event['time'],event['utc_offset'])
+		if event['duration']
+			doc.data['dateEnd'] = convertDate(event['time']+event['duration'],event['utc_offset'])
+		end
+		if event['venue']
+			doc.data['location'] = "#{event['venue']['name']}<br>#{event['venue']['address_1']}"
+		end
+		doc.data['link'] = event['event_url']
+		doc.data['organizer'] = event['group']['name']
+		doc.data['category'] = event['group']['name'].downcase.gsub(/\s+|[():]/, "")
+		doc.data['excerpt'] = event['description']
+		return doc
+  	end
+
   	def generate(site)
   		@site = site
-  		collection = @site.collections["events"]
+  		@collection = @site.collections["events"]
 
 		MeetupClient.configure do |config|
 		  keyPath = "#{@site.source}/meetup_api_key"
 		  config.api_key = ENV['MEETUP_API_KEY'] || File.file?(keyPath) && File.read(keyPath) || return
 		end
 
-		meetup_api = MeetupApi.new
+		@meetup_api = MeetupApi.new
 
 		@@meetup_groups.each do |organizer|
-			maxRetryTimeout = 50
-			initialRetries = 5
-			begin
-				retries ||= initialRetries
-				events = meetup_api.events({ group_urlname: organizer })
-				next if !events.has_key?('results')
-			rescue Exception => e
-				if retries > 0
-					sleepTimeout = maxRetryTimeout - (maxRetryTimeout / initialRetries * retries)
-					Jekyll.logger.warn("Warning:", "The MeetupApi failed for \"#{organizer}\" retrying #{retries} times before giving up… Sleeping for #{sleepTimeout} seconds")
-					sleep(sleepTimeout)
-					retries -= 1
-					retry
-				else
-					Jekyll.logger.error("Error:", "No more retries left, we're going down 💥")
-					raise
-				end
-			end
+			events = getMeetUpGroupEvents(organizer)
+			next if !events.has_key?('results')
 				
 			events['results'].each do |event|
-				doc = Jekyll::Document.new('', :site => site, :collection => collection)
-				doc.data['title'] = event['name']
-				doc.data['dateStart'] = convertDate(event['time'],event['utc_offset'])
-				if event['duration']
-					doc.data['dateEnd'] = convertDate(event['time']+event['duration'],event['utc_offset'])
-				end
-				if event['venue']
-					doc.data['location'] = "#{event['venue']['name']}<br>#{event['venue']['address_1']}"
-				end
-				doc.data['link'] = event['event_url']
-				doc.data['organizer'] = event['group']['name']
-				doc.data['category'] = event['group']['name'].downcase.gsub(/\s+|[():]/, "")
-				doc.data['excerpt'] = event['description']
-				collection.docs << doc
+				doc = generateEventDocument(event)
+				@collection.docs << doc
 			end
 		end
   	end
